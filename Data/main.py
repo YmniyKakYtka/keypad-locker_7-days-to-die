1698f75d74b409b6808609a2dc4f9e556ab39030
YmniyKakYtka <jmaster21042005@mail.ru>
2022-10-21 22:03:37 +0300

    changing-2

--- Data/main.py
+++ Data/main.py
@@ -22,39 +22,37 @@ print(f"Откройте окно с игрой, убрав окно консо�
 
 wait(startButton)
 
-print(pag.position())
+pag.moveTo(x=825, y=547)
 
-# pag.moveTo(x=825, y=547)
-#
-# for length in range(1, 7):
-#     while True:
-#         password = ''
-#         for i in range(0, length):
-#             password += choice(chars)
-#         if password not in wrongPasswords:
-#
-#             with open('../passwords.csv', 'a', encoding='utf-8') as file:
-#                 writer = csv.writer(file)
-#                 writer.writerow(
-#                     (
-#                         password
-#                     )
-#                 )
-#
-#             pag.click()
-#
-#             write(password, 0.01)
-#             send('enter')
-#
-#             if password != correctPassword:
-#                 wrongPasswords.add(password)
-#             else:
-#                 break
-#
-#         if is_pressed(stopButton):
-#             exit(0)
-#
-#         if is_pressed('delete') == True:
-#             break
-#
-#     print(f'{password} is correct')
+for length in range(1, 7):
+    while True:
+        password = ''
+        for i in range(0, length):
+            password += choice(chars)
+        if password not in wrongPasswords:
+
+            with open('../passwords.csv', 'a', encoding='utf-8') as file:
+                writer = csv.writer(file)
+                writer.writerow(
+                    (
+                        password
+                    )
+                )
+
+            pag.click()
+
+            write(password, 0.01)
+            send('enter')
+
+            if password != correctPassword:
+                wrongPasswords.add(password)
+            else:
+                break
+
+        if is_pressed(stopButton):
+            exit(0)
+
+        if is_pressed('delete') == True:
+            break
+
+    print(f'{password} is correct')
