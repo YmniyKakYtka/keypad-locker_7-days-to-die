33ce7b72bdbb5e7e3e8e3368ce6afae96707ccd9
YmniyKakYtka <jmaster21042005@mail.ru>
2022-10-21 21:49:26 +0300

    changing-2

--- Data/main.py
+++ Data/main.py
@@ -20,37 +20,41 @@ with open('../passwords.csv', 'w', encoding='utf-8') as file:
 
 print(f"Откройте окно с игрой, убрав окно консоли и нажмите {startButton} для того чтобы начать\nДля того чтобы завершить процесс преждевременно - зажмите {stopButton}")
 
-pag.moveTo(x=825, y=547)
+wait(startButton)
 
-for length in range(1, 7):
-    while True:
-        password = ''
-        for i in range(0, length):
-            password += choice(chars)
-        if password not in wrongPasswords:
+print(pag.position())
 
-            with open('../passwords.csv', 'a', encoding='utf-8') as file:
-                writer = csv.writer(file)
-                writer.writerow(
-                    (
-                        password
-                    )
-                )
-
-            pag.click()
-
-            write(password, 0.01)
-            send('enter')
-
-            if password != correctPassword:
-                wrongPasswords.add(password)
-            else:
-                break
-
-        if is_pressed(stopButton):
-            exit(0)
-
-        if is_pressed('delete') == True:
-            break
-
-    print(f'{password} is correct')
+# pag.moveTo(x=825, y=547)
+#
+# for length in range(1, 7):
+#     while True:
+#         password = ''
+#         for i in range(0, length):
+#             password += choice(chars)
+#         if password not in wrongPasswords:
+#
+#             with open('../passwords.csv', 'a', encoding='utf-8') as file:
+#                 writer = csv.writer(file)
+#                 writer.writerow(
+#                     (
+#                         password
+#                     )
+#                 )
+#
+#             pag.click()
+#
+#             write(password, 0.01)
+#             send('enter')
+#
+#             if password != correctPassword:
+#                 wrongPasswords.add(password)
+#             else:
+#                 break
+#
+#         if is_pressed(stopButton):
+#             exit(0)
+#
+#         if is_pressed('delete') == True:
+#             break
+#
+#     print(f'{password} is correct')
